66a8dea79f7038a1e6dbc0c18ae66bbfd95b24c0
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 23:08:10 +0300

    Update Zoo.java

--- src/com/company/Zoo.java
+++ src/com/company/Zoo.java
@@ -5,15 +5,18 @@ import java.util.ArrayList;
 public class Zoo {
     private String Name;
     private ArrayList<Cage> Cages;
-    private int animalsNumber;
-
-    public String getName(){return Name;}
-    public ArrayList<Cage> getCages(){return Cages;}
-    public int getAnimalsNumber(){return animalsNumber;}
 
     public void addCage(Cage cage){
         Cages.add(cage);
-        animalsNumber += cage.getAnimalNumber();
         System.out.println("Cage added successfully");
+    }
+
+    public int getAnimalsNumber(){
+        int sum = 0;
+        for (Cage cage:Cages
+             ) {
+            sum += cage.getAnimalNumber();
+        }
+        return sum;
     }
 }

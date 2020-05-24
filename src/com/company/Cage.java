70ee534dc1433855037b062604fc1638d249c609
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 22:35:08 +0300

    Update Cage.java

--- src/com/company/Cage.java
+++ src/com/company/Cage.java
@@ -5,18 +5,22 @@ import Animals.Animal;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Cage{
+public class Cage<T extends Animal>{
     private int Capacity;
     private UUID ID;
-    private ArrayList<Animal> animalsInCage;
+    private ArrayList<T> animalsInCage;
 
     public Cage(int capacity){
         this.Capacity = capacity;
         this.ID = UUID.randomUUID();
-        this.animalsInCage = new ArrayList<Animal>();
+        this.animalsInCage = new ArrayList<T>();
     }
 
     public int getAnimalNumber(){
         return animalsInCage.size();
     }
+
+    public boolean isThisAnimalInCage(T animal){return animalsInCage.contains(animal);}
+
+
 }

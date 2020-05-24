adec70d1f8b71e52946e73692057a7f31d8b4c89
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 22:43:49 +0300

    Delete AnimalAlreadyInCage.java

--- src/Exeptions/AnimalAlreadyInCage.java
+++
@@ -1,8 +0,0 @@
-package Exeptions;
-
-public class AnimalAlreadyInCage extends IllegalArgumentException {
-    @Override
-    public void printStackTrace() {
-        System.out.println("This animal is already in this cage!");
-    }
-}

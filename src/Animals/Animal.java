5f5eab53038432fe0126aef10a38470908e72ce1
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 23:07:58 +0300

    Update Animal.java

--- src/Animals/Animal.java
+++ src/Animals/Animal.java
@@ -3,13 +3,14 @@ package Animals;
 public class Animal {
     private String Name;
     private int Age;
-    private String Type;
     private boolean Predator;
 
     public Animal(String name, int age){
         this.Name = name;
         this.Age = age;
     }
+
+    protected void setPredator(boolean predator){Predator = predator;}
 
     @Override
     public String toString() {
